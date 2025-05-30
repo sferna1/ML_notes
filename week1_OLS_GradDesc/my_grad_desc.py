8fe87436c74d7c53a39f38a2c7f5ddbf9c9d8dfb
Sarah Fernandes <kawaii@Sarahs-MacBook-Pro.local>
2025-05-30 19:18:06 -0400

    Optimized for loop and added intercept, b0.

--- week1_OLS_GradDesc/my_grad_desc.py
+++ week1_OLS_GradDesc/my_grad_desc.py
@@ -1,22 +1,27 @@
 def mygradesc(x, y, learning_rate, iterations):
-	b = 5 #(1) initialize beta
-	n = len(x) #sets n equal to how many data points are in set
-	assert len(x) == len(y) #ensure that x and y share the same length
-	loss_history = [] #creates a new list
-	for i in range(iterations): 
-	# calculate the loss for a value of b
-		loss_sum = 0
-		for j in range(n):
-			loss_sum+= (b*x[j]-y[j])**2
-		total_loss=(1/n)*loss_sum
-	#calculate the derivative of the loss function
-		deriv_loss = 0
-		for k in range(n):
-			deriv_loss+=(b*x[j]-y[j])
-		total_deriv = (1/n)*(2*b)*deriv_loss
-		b = b - learning_rate*total_deriv
-		loss_history.append(total_loss) #adds the current value of 
-# total loss to the list, continues until iterations have reached
+    b1 = 5  # (1a) initialize beta
+    b0 = 5 #(1b) initialize the slope intercept
+    n = len(x)  # sets n equal to how many data points are in set
+    assert len(x) == len(y)  # ensure that x and y share the same length
+    loss_history = []  # creates a new list
 
-	return b, loss_history
+    for i in range(iterations): 
+        # calculate the loss for a value of b
+        loss_sum = 0
+        deriv_b1 = 0
+        deriv_b0 = 0
+        for j in range(n):
+            obs_error = (b0*1 + b1 * x[j]) - y[j]
+            loss_sum += obs_error ** 2
+            deriv_b1 += obs_error*x[j]
+            deriv_b0 += obs_error
+        total_loss = (1 / n) * loss_sum
 
+        # calculate the derivative of the loss function
+        total_deriv_b1 = (1 / n)*2* deriv_b1
+        total_deriv_b0 = (1 / n)*2* deriv_b0
+        b1 = b1 - learning_rate * total_deriv_b1
+        b0 = b0 - learning_rate * total_deriv_b0
+        loss_history.append(total_loss)  # adds the current value of total loss to the list
+
+    return b0, b1, loss_history
